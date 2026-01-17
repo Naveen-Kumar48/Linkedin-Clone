a81dd0274d06019186c49834a2fa41b05da5a3ff
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-01-17 12:06:10 +0530

    feat: Implement post, comment, and like functionalities with dedicated routes and controllers.
    BACKEND COMPLETED

--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
-import { createPost, getAllPosts, deletePost} from '../controllers/posts.controller.js';
+import { createPost, getAllPosts, deletePost, commentPost, getCommentsByPost, delete_comment_of_user, increment_likes} from '../controllers/posts.controller.js';
+
 import multer from 'multer';
 
 const router = Router();
@@ -18,6 +19,10 @@ const upload = multer({ storage: Storage });
 router.route('/post').post(upload.single('media'), createPost);
 router.route('/posts').get(getAllPosts);
 router.route('/delete_post').post(deletePost)
+router.route('/comment').post(commentPost)
+router.route('/get_comments').get(getCommentsByPost)
+router.route('/delete_comment').delete(delete_comment_of_user)
+router.route('/increment_post_likes').post(increment_likes)
 
 export default router;
 
